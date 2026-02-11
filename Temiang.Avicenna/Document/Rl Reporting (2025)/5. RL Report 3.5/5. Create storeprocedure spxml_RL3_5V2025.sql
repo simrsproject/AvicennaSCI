5809cb77e6c4be53a5be99f21f1cbc0b3cfefb91
LuthfiNanda <luthfinanda11@gmail.com>
2026-02-11 11:05:06 +0700

    1.  Replace telerik sama sp dan nambah excel di rl 3.5 v2025

--- Temiang.Avicenna/Document/Rl Reporting (2025)/5. RL Report 3.5/5. Create storeprocedure spxml_RL3_5V2025.sql
+++ Temiang.Avicenna/Document/Rl Reporting (2025)/5. RL Report 3.5/5. Create storeprocedure spxml_RL3_5V2025.sql
@@ -1,212 +1,143 @@
-/************************************************************      
- * Code formatted by SoftTree SQL Assistant © v11.3.277      
- * Time: 16/03/2025 18.16.52      
- ************************************************************/      
-      
-CREATE PROCEDURE spxml_RL3_5V2025(@RlTxReportNo VARCHAR(20))      
-AS      
-SET NOCOUNT ON           
-       
-BEGIN      
- --DECLARE @RlTxReportNo VARCHAR(20) = 'RL/250314-0004';            
-  IF 1 = 0        BEGIN            SET FMTONLY OFF        END              
- DECLARE @PeriodYear           INT,      
-         @PeriodMonthStart     INT,      
-         @PeriodMonthEnd       INT,      
-         @city                 VARCHAR(20),      
-         @HealthcareName       VARCHAR(90),      
-         @HospitalCode         VARCHAR(20),      
-         @ProvincesCode        VARCHAR(20);      
-       
- SELECT @city = h.City,      
-        @HealthcareName     = h.HealthcareName,      
-        @HospitalCode       = h.HospitalCode,      
-        @ProvincesCode      = h.ProvincesCode      
- FROM   Healthcare AS h WITH(NOLOCK);      
-       
- --SELECT * FROM Healthcare AS h      
-       
-       
- SELECT @PeriodYear = rtrv.PeriodYear,      
-        @PeriodMonthStart     = rtrv.PeriodMonthStart,      
-        @PeriodMonthEnd       = rtrv.PeriodMonthEnd      
- FROM   RlTxReportV2025 AS rtrv WITH(NOLOCK)      
- WHERE  rtrv.RlTxReportNo = @RlTxReportNo;      
-       
-       
- DROP TABLE       
- IF EXISTS #ServiceUnits      
-     ;      
-       
- CREATE TABLE #ServiceUnits      
- (      
-  ServiceUnitID VARCHAR(40)      
- );      
- INSERT INTO #ServiceUnits      
- SELECT su.ServiceUnitID      
- FROM   ServiceUnit su   WITH(NOLOCK)      
-        JOIN AppParameter ap   WITH(NOLOCK)      
-             ON  su.DepartmentID = ap.ParameterValue      
- WHERE  ap.ParameterID IN ('EmergencyDepartmentID', 'OutPatientDepartmentID');      
-       
- DECLARE @jumlahharibukapolilakidalamkota INT,      
-         @jumlahharibukapoliwanitadalamkota INT,      
-         @jumlahharibukapolilakiluarkota INT,      
-         @jumlahharibukapoliwanitaluarkota INT,      
-         @JmlPoli INT;      
-       
- -- Hitung jumlah hari buka poli berdasarkan gender dan lokasi      
- WITH HariBuka AS (      
-     SELECT p.Sex,      
-            p.City,      
-            COUNT(DISTINCT psd.ScheduleDate) AS JumlahHari      
-     FROM   ParamedicScheduleDate psd   WITH(NOLOCK)      
-            JOIN Registration r   WITH(NOLOCK)      
-                 ON  psd.ServiceUnitID = r.ServiceUnitID      
-            JOIN Patient p   WITH(NOLOCK)      
-                 ON  r.PatientID = p.PatientID      
-     WHERE  psd.ServiceUnitID IN (SELECT ServiceUnitID      
-                                  FROM   #ServiceUnits)      
-            AND MONTH(psd.ScheduleDate) BETWEEN @PeriodMonthStart AND @PeriodMonthEnd      
-            AND YEAR(psd.ScheduleDate) = @PeriodYear      
-     GROUP BY      
-            p.Sex,      
-            p.City      
- )      
-       
-       
-       
- SELECT @jumlahharibukapolilakidalamkota = SUM(      
-            CASE       
-                 WHEN Sex = 'M'      
-                      AND City     = @city THEN JumlahHari      
-                     ELSE 0      
-                END      
-        ),      
-        @jumlahharibukapoliwanitadalamkota = SUM(      
-            CASE       
-                 WHEN Sex = 'F'      
-                      AND City     = @city THEN JumlahHari      
-                     ELSE 0      
-                END      
-        ),      
-        @jumlahharibukapolilakiluarkota = SUM(      
-            CASE       
-                 WHEN Sex = 'M'      
-                      AND City <> @city THEN JumlahHari      
-                     ELSE 0      
-                END      
-        ),      
-        @jumlahharibukapoliwanitaluarkota = SUM(      
-            CASE       
-                 WHEN Sex = 'F'      
-                      AND City <> @city THEN JumlahHari      
-                     ELSE 0      
-                END      
-        )      
- FROM   HariBuka;      
-       
- -- Hitung jumlah poli aktif      
- SELECT @JmlPoli = COUNT(ServiceUnitID)      
- FROM   #ServiceUnits;      
-       
-       
- DECLARE @TotKunjunganPriaDalam       INT,      
-         @TotKunjunganWanitaDalam     INT,      
-         @TotKunjunganPriaLuar        INT,      
-         @TotKunjunganWanitaLuar      INT;      
-       
- SELECT @TotKunjunganPriaDalam = rtrv.JumlahLaki,      
-        @TotKunjunganWanitaDalam     = rtrv.JumlahPerempuan,      
-        @TotKunjunganPriaLuar        = rtrv.JumlahLaki2,      
-        @TotKunjunganWanitaLuar      = rtrv.JumlahPerempuan2      
- FROM   RlTxReport3_5V2025 AS rtrv WITH(NOLOCK)      
- WHERE  rtrv.RlTxReportNo = @RlTxReportNo      
-        AND rtrv.RlMasterReportItemID = '206';      
-       
-       
-       
-       
- SELECT 'RL 3.5.Rekapitulasi Kunjungan' ReportName,      
-        'Periode : ' + asri.Note + ' s/d ' + asri2.Note + ' ' + b.PeriodYear AS Periode,      
-        @city                      AS City,      
-        @HealthcareName            AS HealthcareName,      
-        @HospitalCode              AS HospitalCode,      
-        @ProvincesCode             AS HospitalCode,      
-        b.PeriodYear,      
-        c.RlMasterReportItemCode   AS 'No',      
-        c.RlMasterReportItemName   AS 'JenisKegiatan',      
-        a.JumlahLaki,      
-        a.JumlahPerempuan,      
-        a.JumlahLaki2,      
-        a.JumlahPerempuan2,      
-        a.Jumlah,      
-        @jumlahharibukapolilakidalamkota AS jumlahharibukapolilakidalamkota,      
-        @jumlahharibukapoliwanitadalamkota AS jumlahharibukapoliwanitadalamkota,      
-        @jumlahharibukapolilakiluarkota AS jumlahharibukapolilakiluarkota,      
-        @jumlahharibukapoliwanitaluarkota AS jumlahharibukapoliwanitaluarkota,      
-        @JmlPoli                   AS JmlPoli,      
-        @jumlahharibukapolilakidalamkota / @JmlPoli AS RataRataHariPoliklinilBukaDalamKotaPria,      
-        @jumlahharibukapoliwanitadalamkota / @JmlPoli AS RataRataHariPoliklinilBukaDalamKotaWanita,      
-        @jumlahharibukapolilakiluarkota / @JmlPoli AS RataRataHariPoliklinilBukaluarKotaPria,      
-        @jumlahharibukapoliwanitaluarkota / @JmlPoli AS RataRataHariPoliklinilBukaluarKotaWanita,      
-        @TotKunjunganPriaDalam     AS TotKunjunganPriaDalam,      
-        @TotKunjunganWanitaDalam   AS TotKunjunganWanitaDalam,      
-        @TotKunjunganPriaLuar      AS TotKunjunganPriaLuar,      
-        @TotKunjunganWanitaLuar    AS TotKunjunganWanitaLuar,      
-        ISNULL(      
-            @TotKunjunganPriaDalam / NULLIF((@jumlahharibukapolilakidalamkota / @JmlPoli), 0),      
-            0      
-        )                          AS satu,      
-        ISNULL(      
-            @TotKunjunganWanitaDalam / NULLIF((@jumlahharibukapoliwanitadalamkota / @JmlPoli), 0),      
-            0      
-        )                          AS dua,      
-        ISNULL(      
-            @TotKunjunganPriaLuar / NULLIF((@jumlahharibukapolilakiluarkota / @JmlPoli), 0),      
-            0      
-        )                          AS tiga,      
-        ISNULL(      
-            @TotKunjunganWanitaLuar / NULLIF((@jumlahharibukapoliwanitaluarkota / @JmlPoli), 0),      
-            0      
-        )                          AS empat      
-        ,      
-              
-              
-              
-        ISNULL(      
-            @TotKunjunganPriaDalam / NULLIF((@jumlahharibukapolilakidalamkota / @JmlPoli), 0),      
-            0      
-        ) +      
-        ISNULL(      
-            @TotKunjunganWanitaDalam / NULLIF((@jumlahharibukapoliwanitadalamkota / @JmlPoli), 0),      
-            0      
-        )                          +      
-        ISNULL(      
-            @TotKunjunganPriaLuar / NULLIF((@jumlahharibukapolilakiluarkota / @JmlPoli), 0),      
-            0      
-        )                        +      
-        ISNULL(      
-            @TotKunjunganWanitaLuar / NULLIF((@jumlahharibukapoliwanitaluarkota / @JmlPoli), 0),      
-            0      
-        )                        AS lima      
-              
-              
-              
- FROM   RlTxReport3_5V2025         AS a WITH(NOLOCK)      
-        LEFT JOIN RlTxReportV2025  AS b WITH(NOLOCK)      
-             ON  b.RlTxReportNo = a.RlTxReportNo      
-        LEFT JOIN RlMasterReportItemV2025 AS c WITH(NOLOCK)      
-             ON  c.RlMasterReportItemID = a.RlMasterReportItemID      
-        LEFT JOIN AppStandardReferenceItem asri WITH(NOLOCK)      
-             ON  asri.StandardReferenceID = 'MonthID'      
-             AND asri.ReferenceID = b.PeriodMonthStart      
-        LEFT JOIN AppStandardReferenceItem asri2 WITH(NOLOCK)      
-             ON  asri2.StandardReferenceID = 'MonthID'      
-             AND asri2.ReferenceID = b.PeriodMonthEnd      
- WHERE  a.RlTxReportNo = @RlTxReportNo      
- ORDER BY      
-        c.RlMasterReportItemNo       
-       
- DROP TABLE #ServiceUnits;      
-END
+/************************************************************
+ * Code formatted by SoftTree SQL Assistant © v11.3.277
+ * Time: 11/02/2026 10.25.48
+ ************************************************************/
+
+/************************************************************    
+ * Code formatted by SoftTree SQL Assistant © v11.3.277    
+ * Time: 22/11/2025 18.18.54    
+ ************************************************************/    
+    
+CREATE OR ALTER PROCEDURE spxml_RL3_5V2025(@RlTxReportNo VARCHAR(20))
+AS
+	SET NOCOUNT ON 
+	
+	
+	--DECLARE @RlTxReportNo VARCHAR(20) = 'RL/250903-0004';            
+	DECLARE @city                         VARCHAR(30),
+	        @HealthcareName               VARCHAR(MAX),
+	        @ProvincesCode                VARCHAR(30),
+	        @HospitalCode                 VARCHAR(30),
+	        @PeriodYear                   INT,
+	        @PeriodMonthStart             INT,
+	        @PeriodMonthEnd               INT,
+	        @JumlahHariBukaPoliklinik     INT,
+	        @JumlahPoliklinikdiRS         INT,
+	        @TotKunjunganPriaDalam        INT,
+	        @TotKunjunganPriaLuar         INT,
+	        @TotKunjunganWanitaLuar       INT,
+	        @TotKunjunganWanitaDalam      INT    
+	
+	
+	
+	
+	
+	SELECT @city = h.City,
+	       @HealthcareName     = h.HealthcareName,
+	       @HospitalCode       = h.HospitalCode,
+	       @ProvincesCode      = h.ProvincesCode
+	FROM   Healthcare AS h WITH(NOLOCK)    
+	
+	SELECT @TotKunjunganPriaDalam = rtrv.JumlahLaki,
+	       @TotKunjunganPriaLuar        = rtrv.JumlahLaki2,
+	       @TotKunjunganWanitaDalam     = rtrv.JumlahPerempuan,
+	       @TotKunjunganWanitaLuar      = rtrv.JumlahPerempuan2
+	FROM   RlTxReport3_5V2025 AS rtrv
+	WHERE  rtrv.RlMasterReportItemID = '206'
+	       AND rtrv.RlTxReportNo = @RlTxReportNo    
+	
+	
+	SELECT @PeriodYear = rtrv.PeriodYear,
+	       @PeriodMonthStart     = rtrv.PeriodMonthStart,
+	       @PeriodMonthEnd       = rtrv.PeriodMonthEnd
+	FROM   RlTxReportV2025 AS rtrv WITH(NOLOCK)
+	WHERE  rtrv.RlTxReportNo = @RlTxReportNo    
+	
+	DECLARE @ServiceUnit TABLE (ServiceUnitID VARCHAR(255))    
+	
+	INSERT INTO @ServiceUnit
+	  (
+	    ServiceUnitID
+	  )
+	SELECT LTRIM(RTRIM(x.XmlCol.value('.', 'varchar(50)')))
+	FROM   (
+	           SELECT CAST(
+	                      '<i>' +
+	                      REPLACE(
+	                          REPLACE(string_agg(rmriv.ParameterValue, ','), ',,', ','),
+	                          ',',
+	                          '</i><i>'
+	                      ) 
+	                      + '</i>' AS XML
+	                  ) AS XMLDATA
+	           FROM   RlMasterReportItemV2025 rmriv
+	           WHERE  rmriv.RlMasterReportID = '5'
+	       ) AS r
+	       CROSS APPLY r.XmlData.nodes('/i') AS x(XmlCol); 
+	WITH CTE_1 AS (
+	    SELECT DISTINCT ps.ServiceUnitID,
+	           ps.ScheduleDate,
+	           MONTH(ps.ScheduleDate)  AS Bulan
+	    FROM   ParamedicScheduleDate   AS ps
+	    WHERE  ps.PeriodYear = @PeriodYear
+	           AND MONTH(ps.ScheduleDate) BETWEEN @PeriodMonthStart AND @PeriodMonthEnd
+	           AND ps.ServiceUnitID IN (SELECT *
+	                                    FROM   @ServiceUnit AS su
+	                                    WHERE  su.ServiceUnitID <> '')
+	)    
+	
+	SELECT @JumlahHariBukaPoliklinik = COUNT(*)
+	FROM   CTE_1 AS c
+	GROUP BY
+	       c.Bulan    
+	
+	SELECT @JumlahPoliklinikdiRS = COUNT(*)
+	FROM   @ServiceUnit AS su;;    
+	
+	
+	SELECT 'RL 3.5 Rekapitulasi Kunjungan' ReportName,
+	       CASE 
+	            WHEN b.PeriodMonthStart = b.PeriodMonthEnd THEN 'Periode : ' + asri.Note + ' ' + b.PeriodYear
+	            ELSE 'Periode : ' + asri.Note + ' s/d ' + asri2.ItemName + ' ' + b.PeriodYear
+	       END                         AS Periode,
+	       asri.Note                   AS Bulan,
+	       @HealthcareName             AS HealthcareName,
+	       @HospitalCode               AS HospitalCode,
+	       @ProvincesCode              AS ProvincesCode,
+	       @City                       AS City,
+	       b.PeriodYear,
+	       c.RlMasterReportItemCode    AS 'No',
+	       c.RlMasterReportItemName    AS 'JenisKegiatan',
+	       a.JumlahLaki,
+	       a.JumlahPerempuan,
+	       a.JumlahLaki2,
+	       a.JumlahPerempuan2,
+	       a.Jumlah,
+	       @JumlahHariBukaPoliklinik   AS JumlahHariBukaPoliklinik,
+	       @JumlahPoliklinikdiRS       AS JumlahPoliklinikdiRS,
+	       @JumlahHariBukaPoliklinik / @JumlahPoliklinikdiRS AS RataRataHariPoliklinikBuka,
+	       @TotKunjunganPriaDalam      AS TotKunjunganPriaDalam,
+	       @TotKunjunganWanitaDalam    AS TotKunjunganWanitaDalam,
+	       @TotKunjunganPriaLuar       AS TotKunjunganPriaLuar,
+	       @TotKunjunganWanitaLuar     AS TotKunjunganWanitaLuar,
+	       @TotKunjunganPriaDalam / (@JumlahHariBukaPoliklinik / @JumlahPoliklinikdiRS) AS RataA,
+	       @TotKunjunganWanitaDalam / (@JumlahHariBukaPoliklinik / @JumlahPoliklinikdiRS) AS RataB,
+	       @TotKunjunganPriaLuar / (@JumlahHariBukaPoliklinik / @JumlahPoliklinikdiRS) AS RataC,
+	       @TotKunjunganWanitaLuar / (@JumlahHariBukaPoliklinik / @JumlahPoliklinikdiRS) AS RataD
+	FROM   RlTxReport3_5V2025          AS a WITH(NOLOCK)
+	       INNER JOIN RlTxReportV2025  AS b WITH(NOLOCK)
+	            ON  b.RlTxReportNo = a.RlTxReportNo
+	       INNER JOIN RlMasterReportItemV2025 AS c WITH(NOLOCK)
+	            ON  c.RlMasterReportItemID = a.RlMasterReportItemID
+	       INNER JOIN AppStandardReferenceItem asri WITH(NOLOCK)
+	            ON  asri.StandardReferenceID = 'MonthID'
+	            AND asri.ReferenceID = b.PeriodMonthStart
+	       INNER JOIN AppStandardReferenceItem asri2 WITH(NOLOCK)
+	            ON  asri2.StandardReferenceID = 'MonthID'
+	            AND asri2.ReferenceID = b.PeriodMonthEnd
+	WHERE  a.RlTxReportNo = @RlTxReportNo
+	ORDER BY
+	       c.RlMasterReportItemNo             
+                  
+                  
+                  
