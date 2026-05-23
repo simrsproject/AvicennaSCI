a4accb3d977431372fdd357f00d2de8e1826986c
Wiliam Decosta <wiliamdecosta@gmail.com>
2026-05-23 19:21:45 +0700

    up

---
+++ Temiang.Avicenna/Controllers/AntrianSatuTiketController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using Temiang.Avicenna.Common;
+
+namespace Temiang.Avicenna.Controllers
+{
+    public class AntrianSatuTiketController : Controller
+    {
+
+        public bool IsLoggedIn()
+        {
+            return !string.IsNullOrEmpty(AppSession.UserLogin.UserID);
+        }
+
+        public ActionResult Index()
+        {
+            if(!IsLoggedIn())
+            {
+                RedirectToAction("Login");
+            }
+
+            string redirectUrl = Request.QueryString["redirectUrl"];
+            redirectUrl = redirectUrl.Replace("{uid}", AppSession.UserLogin.UserID);
+
+            return Redirect(redirectUrl);
+        }   
+    }
+}
