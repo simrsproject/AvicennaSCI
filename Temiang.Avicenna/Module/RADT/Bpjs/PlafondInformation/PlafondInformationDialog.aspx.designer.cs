227e58ca234aa2e95f7f012e920b0e0f6b636414
Aminnudin <aminnudin.bdg@gmail.com>
2026-08-20 14:25:19 +0700

    feat: add MPP note to plafond information

--- Temiang.Avicenna/Module/RADT/Bpjs/PlafondInformation/PlafondInformationDialog.aspx.designer.cs
+++ Temiang.Avicenna/Module/RADT/Bpjs/PlafondInformation/PlafondInformationDialog.aspx.designer.cs
@@ -303,6 +303,15 @@ namespace Temiang.Avicenna.Module.RADT.Bpjs
         protected global::Telerik.Web.UI.RadTextBox txtSuggestion;
 
         /// <summary>
+        /// txtMppNote control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Telerik.Web.UI.RadTextBox txtMppNote;
+
+        /// <summary>
         /// txtBpjsCoverageFormula control.
         /// </summary>
         /// <remarks>
