fd1abbca3a6c1026bbe079aa42fbde9232bf427c
Aminnudin <aminnudin.bdg@gmail.com>
2026-08-24 09:23:00 +0700

    fix: show incomplete job order realization items

--- Temiang.Avicenna/Module/Charges/ServiceUnit/JobOrderRealisation/JobOrderRealisationDetail.aspx.cs
+++ Temiang.Avicenna/Module/Charges/ServiceUnit/JobOrderRealisation/JobOrderRealisationDetail.aspx.cs
@@ -3898,7 +3898,12 @@ namespace Temiang.Avicenna.Module.Charges
                     (
                         query.TransactionNo == Request.QueryString["joNo"],
                         query.IsVoid == false,
-                        query.IsBillProceed == false,
+                        query.Or(
+                            query.IsBillProceed == false,
+                            query.IsBillProceed == true
+                            & query.Or(query.IsOrderRealization.IsNull(), query.IsOrderRealization == false)
+                            & query.RealizationDateTime.IsNull()
+                        ),
                         query.NotExists(tci)
                     );
                 query.Where(query.ParentNo == string.Empty);
