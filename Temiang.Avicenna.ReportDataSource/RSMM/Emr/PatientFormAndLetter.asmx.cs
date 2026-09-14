ff3c06b5962b20737765ff900e7159f89023a8f2
Wiliam Decosta <wiliamdecosta@gmail.com>
2026-09-14 21:50:50 +0700

    Modify DPJP Team, MedicalProcedures, and Medications respons to prevents errors in Surat Balasan Rujukan Klinik Silih Asih

--- Temiang.Avicenna.ReportDataSource/RSMM/Emr/PatientFormAndLetter.asmx.cs
+++ Temiang.Avicenna.ReportDataSource/RSMM/Emr/PatientFormAndLetter.asmx.cs
@@ -1730,7 +1730,7 @@ namespace Temiang.Avicenna.ReportDataSource.RSMM.Emr
                 
                 if(string.IsNullOrEmpty(referralName) && string.IsNullOrEmpty(referralAddress))
                 {
-                    if (!string.IsNullOrWhiteSpace(reg.ReferralID))
+                    if (!string.IsNullOrEmpty(reg.ReferralID))
                     {
                         var rfr = new Referral();
                         rfr.LoadByPrimaryKey(reg.ReferralID);
@@ -1752,8 +1752,8 @@ namespace Temiang.Avicenna.ReportDataSource.RSMM.Emr
                     PastMedicalHist = medsum.PastMedicalHistory,
                     PhysicalExam = medsum.PhysicalExam,
                     AncillaryExam = medsum.AncillaryExam,
-                    MedicalProcedures = medsum.MedicalProcedures.Replace("â€¢", "\u2022"),
-                    Medications = medsum.Medications.Replace("â€¢", "\u2022"),
+                    MedicalProcedures = string.IsNullOrEmpty(medsum.MedicalProcedures) ? string.Empty : medsum.MedicalProcedures.Replace("â€¢", "\u2022"),
+                    Medications =  string.IsNullOrEmpty(medsum.Medications) ? string.Empty : medsum.Medications.Replace("â€¢", "\u2022"),
                     KondisiPulang = StandardReference.GetItemName(AppEnum.StandardReference.DischargeCondition, medsum.SRDischargeCondition),
                     SuggestionFollowUp = medsum.SuggestionFollowUp,
                     AlasanPulang = StandardReference.GetItemName(AppEnum.StandardReference.DischargeMethod, medsum.SRDischargeMethod),
